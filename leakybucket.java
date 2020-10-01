db5b480e641d23ca098e1c543f0dde6d34f37c6c
gpakshay <71272125+gpakshay@users.noreply.github.com>
2020-10-01 10:46:57 +0530

    Create leakybucket.java

---
+++ leakybucket.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Leaky
+public static int bucketSize = 1000;
+public static int outputRate = 100;
+public static void sendPacket(int pktSize)
+if (pktSize > bucketSize)
+System.out.println("Bucket OverFlow");
+else
+while (pktSize > outputRate)
+System.out.println(outputRate + " bytes of packet is sent");
+pktSize = pktSize - outputRate;
+System.outprintin(pktSize + " bytes of packet is sent");
+public static void main(Stringl] args)
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter the no of packets: ");
+numpackets = scanner.nextlnt()
+if (numpackets > 0)
+for (int i = 1; i<= numpackets; i++)
+System.out.print("Enter the packet " +i+ " size : ");
+int pktSize = scanner.nextlnt);
+sendPacket(pktSize);
+else
+System.out.println("No Packets to Send");
