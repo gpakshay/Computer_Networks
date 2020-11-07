a081b69693811a3864a1e9b91df6b880b84d729f
gpakshay <71272125+gpakshay@users.noreply.github.com>
2020-11-07 12:04:12 +0530

    Update crc.java

--- crc.java
+++ crc.java
@@ -1,20 +1,18 @@
-public class CRC16CCITT{
-public static void main(String[] args){
-int crc = OxFFFF;
-int polynomial = Ox 1021; 
-byte[] testBytes = "123456789".getBytes("ASCII");
-byte[] bytes = args[0].getBytes();
-for (byte b : bytes) 
-{
-for (int i = 0; i < 8; i++) 
-{
-boolean bit = ((b >>(7-i) & 1) ==1);
-boolean cl5 = ((crc >> 15 & 1) == 1);
-crc <<= l;
-if (c15^bit) crc^= polynomial;
-}
-}
-crc &= 0xffff;
-StdOut.println("CRC16-CCITT = " +Integer.toHexString(crc));
-}
+public class CRC16CCITT {
+ public static void main(String[] args) {
+ int crc = 0xFFFF; // initial value
+ int polynomial = 0x1021; // 0001 0000 0010 0001 (0, 5, 12)
+ // byte[] testBytes = "123456789".getBytes("ASCII");
+ byte[] bytes = args[0].getBytes();
+ for (byte b : bytes) {
+ for (int i = 0; i < 8; i++) {
+ boolean bit = ((b >> (7-i) & 1) == 1);
+ boolean c15 = ((crc >> 15 & 1) == 1);
+ crc <<= 1;
+ if (c15 ^ bit) crc ^= polynomial;
+ }
+ }
+ crc &= 0xffff;
+ StdOut.println("CRC16-CCITT = " + Integer.toHexString(crc));
+ }
 }
